fe136dd1523525763a69a4df9ffc0508512b88f9
purplevarun <purplevarun@gmail.com>
2021-12-31 18:00:06 +0530

    heroku test

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const path = require("path");
+const port = process.env.PORT || 5000;
+//
+dotenv.config();
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+mongoose.connect(process.env.MONGO_URL, () =>
+	console.log("Cloud DB Connected")
+);
+//
+if (process.env.PRODUCTION === true) {
+	app.use(express.static(path.join(__dirname, "../frontend/build")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.join(__dirname, "../frontend/build"));
+	});
+}
+//
+app.listen(port, () => console.log("Server started at 5000"));

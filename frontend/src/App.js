00b1b7110646c244c76467d4534e4b87a1b1893b
purplevarun <purplevarun@gmail.com>
2022-01-03 00:06:47 +0530

    homepage header footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,25 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
 import "./spacelab.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Homepage from "./pages/Homepage";
+import "./styles/App.css";
 function App() {
-	const [data, setData] = useState("nofing");
 	useEffect(() => {
-		const getData = async () => {
-			const { data } = await axios.get("/api");
-			setData(data);
-		};
-		getData();
+		document.title = "AEC Coding Club";
 	});
 	return (
 		<div className="App">
-			<h1>hello world</h1>
-			<h2>{data}</h2>
+			<Header />
+			<div className="MainBody">
+				<BrowserRouter>
+					<Routes>
+						<Route exact path="/" element={<Homepage />} />
+					</Routes>
+				</BrowserRouter>
+			</div>
+			<Footer />
 		</div>
 	);
 }

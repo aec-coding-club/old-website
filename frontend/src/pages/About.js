f804ac15bb69cd7fc5270c332607709ede9dd5f3
purplevarun <purplevarun@gmail.com>
2022-01-05 20:45:25 -0800

    rebase

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,7 +1,38 @@
 import React from "react";
-
+import "./../styles/About.css";
 const About = () => {
-	return <div></div>;
+	const inaug_poster = "images/drive/inaug_poster.png";
+	return (
+		<div>
+			<div style={{ textAlign: "center", marginTop: "20px" }}>
+				<h5>
+					Managed by Department of Information Technology - Asansol
+					Engineering College
+				</h5>
+				<h6>
+					Est. 11 April, 2021{" "}
+					<button
+						className="btn btn-sm btn-primary"
+						onClick={() => (window.location.href = inaug_poster)}
+					>
+						Click
+					</button>
+				</h6>
+			</div>
+			<div className="box">
+				<h1>Teacher Coordinators</h1>
+				<li>Mr. Sudip Kumar De</li>
+				<li>Dr. Anup Kumar Mukhopadhyay</li>
+				<li>Mr. Biplap Kumar Mondal</li>
+			</div>
+			<div className="box">
+				<h1>Student Coordinators (Active)</h1>
+				<li>Varun Kedia</li>
+				<li>Soumita Hazra</li>
+				<li></li>
+			</div>
+		</div>
+	);
 };
 
 export default About;
